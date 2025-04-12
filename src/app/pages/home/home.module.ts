9d7a9b7fc30a7b9a83688a468f4384f102a541a9
Eze Barrios <eze_barrios_96@hotmail.com>
2025-04-12 20:43:58 -0300

    Boton emergency

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page'
+import { BtnEmergencyComponent } from 'src/app/settings/components/btn-emergency/btn-emergency.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    BtnEmergencyComponent
   ],
   declarations: [HomePage]
 })
