a0df90efb1ede8d6b673f107d673202c9a6621d3
Nicolas Ysidoro Oris <nicolasoris98@gmail.com>
2025-04-13 21:43:16 -0300

    Added ws

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,39 @@
 
 import { Component } from '@angular/core';
 import { AuthService } from './services/auth.service';
+import { environment } from 'src/environments/environment';
+import { ToastController } from '@ionic/angular';
+import { Socket } from 'ngx-socket-io';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: false,
 })
-export class AppComponent {
+export class AppComponent{
+
   public appPages = [
     { title: 'Home', url: '/home', icon: 'home' },
     { title: 'Fotos', url: '/photos', icon: 'images' }
-  ];
-  public labels = ['Perfil'];
-  constructor(public authService: AuthService) {}
+  ]
+
+  public labels = ['Perfil']
+
+  constructor(public authService: AuthService, private toastController: ToastController, private socket: Socket) {
+    this.socket.connect()
+
+    this.socket.fromEvent('message').subscribe(async(message) => {
+      console.log("websocket", message)
+      const toast = await this.toastController.create({
+        message: "¡Persona Detectada!",
+        duration: 1500,
+        position: "top",
+        color: "danger"
+      })
+
+      await toast.present()
+    })
+  }
 
   logout() {
     this.authService.logout()
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http'
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { environment } from 'src/environments/environment';
+
+const config: SocketIoConfig = { url: environment.API_URL, options: {} };
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, SocketIoModule.forRoot(config)],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
